c5bb5d1e21f7d237bb181398c377a961f695fa74
Andy Roberts <djandyr@gmail.com>
2014-07-04 12:51:49 +0100

    Add guzzle to retrieve stories for changelog

--- src/PivotalTracker/ChangeLog.php
+++ src/PivotalTracker/ChangeLog.php
@@ -6,12 +6,28 @@
 
 namespace PivotalTracker;
 
-class ChangeLog {
+use \Guzzle\Http\Exception\ClientErrorResponseException;
+
+class ChangeLog
+{
 
     const PROJECT = 433999;
-    const TOKEN = d26b8fbb80bb4aab5f771fb7833712c;
+    const TOKEN = 'd26b8fbb80bb4aab5f771fb7833712c1';
 
-    public function build() {
-        echo 'hello';
+    public function __construct()
+    {
+        $this->url = sprintf('https://www.pivotaltracker.com/services/v5/projects/%s/stories', self::PROJECT);
+    }
+
+    public function build()
+    {
+
+        $client = new \Guzzle\Http\Client();
+
+        $response = $client->get($this->url, array(
+            'X-TrackerToken' => self::TOKEN
+        ))->send();
+
+        var_dump($response->json());
     }
 }
